bcbcbb4cd2c5d742379aa2bbce66c4478c709a07
lbr <lichangxinlivingshade@gmail.com>
2023-02-27 22:16:43 -0500

    fix

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
 const int SERVER_PORT = 8081;
 const int NF_PORT = 12345;
 const int FILE_MAX_SIZE = 1e8;
-const int SO_SIZE = 55956880;
+const int SO_SIZE = 8752800;
 const std::string SERVER_ADDRESS = "127.0.0.1";
 const std::string SO_NAME_CLIENT = "./so/libnaive_server_interceptor.so";
 const std::string SO_NAME_SERVER = "./so/libnaive_server_interceptor2.so";
